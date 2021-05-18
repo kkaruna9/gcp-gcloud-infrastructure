382047db34423d018300f4cca2973ba7d2b513db
Dinesh.A <a.dinesh1901@gmail.com>
2021-05-18 13:14:03 +0100

    Updated the variable values in double quotes

--- landing-zone-setup/01_lz_environment-variables.sh
+++ landing-zone-setup/01_lz_environment-variables.sh
@@ -11,9 +11,9 @@ echo "
 # sh ./01_lz_environment-variables.sh
 # sh ./02_lz_create-repo.sh
 export LZ_PROJECT_ID=$(gcloud config get-value project)
-export LZ_REPOPREFIX=lz-repo-1
-export LZ_BUCKET_NAME=lz-results
-export LZ_BUCKET_LOCATION=US-EAST1
-export LZ_BUCKET_STORAGE_CLASS=STANDARD
+export LZ_REPOPREFIX="lz-repo-1"
+export LZ_BUCKET_NAME="lz-results"
+export LZ_BUCKET_LOCATION="US-EAST1"
+export LZ_BUCKET_STORAGE_CLASS="STANDARD"
 #git config --global user.email
 #git config --global user.name $GIT-USER-NAME
