5fc80d607754b97fb3c4664156475c48f29d5012
Dinesh.A <a.dinesh1901@gmail.com>
2021-05-18 20:32:37 +0100

    Updated the variable script for echo the variable values

--- landing-zone-setup/01_lz_environment-variables.sh
+++ landing-zone-setup/01_lz_environment-variables.sh
@@ -10,11 +10,19 @@ echo "
 # cd landing-zone-setup
 # sh ./01_lz_environment-variables.sh
 # sh ./02_lz_create-repo.sh
-export LZ_PROJECT_ID=$(gcloud config get-value project)
 export LZ_REPOPREFIX="lz-repo-1"
 export LZ_BUCKET_NAME="lz-results"
 export LZ_BUCKET_LOCATION="US-EAST1"
 export LZ_BUCKET_STORAGE_CLASS="STANDARD"
 export LZ_BUCKET_REPO="lz-artifacts"
+export LZ_PROJECT_ID=$(gcloud config get-value project)
+
+echo $LZ_REPOPREFIX
+echo $LZ_BUCKET_NAME
+echo $LZ_BUCKET_LOCATION
+echo $LZ_BUCKET_STORAGE_CLASS
+echo $LZ_BUCKET_REPO
+echo $LZ_PROJECT_ID
+
 #git config --global user.email
 #git config --global user.name $GIT-USER-NAME
