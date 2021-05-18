bc0ed0c06dc5a7bff4a8dd4a4ba0a444bddc7755
Dinesh.A <a.dinesh1901@gmail.com>
2021-05-18 08:00:55 +0100

    Committing the changes for landingzone infrastructure setup - push source2workshop

--- landing-zone-setup/02_lz_create-repo.sh
+++ landing-zone-setup/02_lz_create-repo.sh
@@ -13,3 +13,59 @@ else
   echo "Failed to create '$LZ_REPOPREFIX-landling-zone' repository"
 
 
+
+gcloud -q source repos create $LZ_REPOPREFIX-gcp-aut-frontend
+if [ $? -eq 0 ]
+then
+  echo "Created '$LZ_REPOPREFIX-gcp-aut-frontend' repository"
+else
+  echo "Failed to create '$LZ_REPOPREFIX-gcp-aut-frontend' repository"
+fi
+
+gcloud -q source repos create $LZ_REPOPREFIX-gcp-aut-backend
+if [ $? -eq 0 ]
+then
+  echo "Created '$LZ_REPOPREFIX-gcp-aut-backend' repository"
+else
+  echo "Failed to create '$LZ_REPOPREFIX-gcp-aut-backend' repository"
+fi
+
+gcloud -q source repos create $LZ_REPOPREFIX-gcp-report-dashboard
+if [ $? -eq 0 ]
+then
+  echo "Created '$LZ_REPOPREFIX-gcp-report-dashboard' repository"
+else
+  echo "Failed to create '$LZ_REPOPREFIX-gcp-report-dashboard' repository"
+fi
+
+gcloud -q source repos create $LZ_REPOPREFIX-gcp-functional-assurance
+if [ $? -eq 0 ]
+then
+  echo "Created '$LZ_REPOPREFIX-gcp-functional-assurance' repository"
+else
+  echo "Failed to create '$LZ_REPOPREFIX-gcp-functional-assurance' repository"
+fi
+
+gcloud -q source repos create $LZ_REPOPREFIX-gcp-tests-performance
+if [ $? -eq 0 ]
+then
+  echo "Created '$LZ_REPOPREFIX-gcp-tests-performance' repository"
+else
+  echo "Failed to create '$LZ_REPOPREFIX-gcp-tests-performance' repository"
+fi
+
+gcloud -q source repos create $LZ_REPOPREFIX-gcp-tests-security
+if [ $? -eq 0 ]
+then
+  echo "Created '$LZ_REPOPREFIX-gcp-tests-security' repository"
+else
+  echo "Failed to create '$LZ_REPOPREFIX-gcp-tests-security' repository"
+fi
+
+gcloud -q source repos create $LZ_REPOPREFIX-gcp-tests-accessibility
+if [ $? -eq 0 ]
+then
+  echo "Created '$LZ_REPOPREFIX-gcp-tests-accessibility' repository"
+else
+  echo "Failed to create '$LZ_REPOPREFIX-gcp-tests-accessibility' repository"
+fi
---
+++ landing-zone-setup/03_push_source2workshop.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+echo "
++----------------------------------------------------------------------------+
+| Please wait as the code is being pushed to your cloud source repositories. |
+| (This will take approx. 3-5 minutes.)                                      |
++----------------------------------------------------------------------------+
+"
+export REPO_PATH="https://source.developers.google.com/p/"$GOOGLE_CLOUD_PROJECT"/r/"
+
+export FRONTEND_REPO=$REPO_PATH$LZ_REPOPREFIX"-gcp-aut-frontend"
+export BACKEND_REPO=$REPO_PATH$LZ_REPOPREFIX"-gcp-aut-backend"
+export DASHBOARD_REPO=$REPO_PATH$LZ_REPOPREFIX"-gcp-report-dashboard"
+export FUNCTIONAL_REPO=$REPO_PATH$LZ_REPOPREFIX"-gcp-functional-assurance"
+export PERFORMANCE_REPO=$REPO_PATH$LZ_REPOPREFIX"-gcp-tests-performance"
+export SECURITY_REPO=$REPO_PATH$LZ_REPOPREFIX"-gcp-tests-security"
+export ACCESSIBILITY_REPO=$REPO_PATH$LZ_REPOPREFIX"-gcp-tests-accessibility"
+
+
+#download all repos
+#gsutil -mq cp -r gs://gd-investments-1000311198.appspot.com/gcpworkshop-code-repos .
+gsutil -mq cp -r gs://$LZ_PROJECT_ID.appspot.com/gcpworkshop-code-repos .
+cd gcpworkshop-code-repos
+
+#frontend repo
+
+cd gcpworkshop-aut-frontend
+git init
+git add .
+git commit -m "init commit"  --quiet
+git remote add origin $FRONTEND_REPO
+git push -u origin master --quiet
+if [ $? -eq 0 ]
+then
+  echo "Code push success on '$LZ_REPOPREFIX-gcp-aut-frontend' repository"
+else
+  echo "Failed to push code into '$LZ_REPOPREFIX-gcp-aut-frontend' repository"
+fi
+cd ..
+
+# backend repo
+
+cd gcpworkshop-aut-backend
+git init
+git add .
+git commit -m "init commit" --quiet
+git remote add origin $BACKEND_REPO
+git push -u origin master --quiet
+if [ $? -eq 0 ]
+then
+  echo "Code push success on '$LZ_REPOPREFIX-gcp-aut-backend' repository"
+else
+  echo "Failed to push code into '$LZ_REPOPREFIX-gcp-aut-backend' repository"
+fi
+cd ..
+
+#dashboard repo
+cd gcpworkshop-report-dashboard
+git init
+git add .
+git commit -m "init commit" --quiet
+git remote add origin $DASHBOARD_REPO
+git push -u origin master --quiet
+if [ $? -eq 0 ]
+then
+  echo "Code push success on '$LZ_REPOPREFIX-gcp-report-dashboard' repository"
+else
+  echo "Failed to push code into '$LZ_REPOPREFIX-gcp-report-dashboard' repository"
+fi
+cd ..
+
+#functional test repo
+cd gcpworkshop-functional-assurance
+git init
+git add .
+git commit -m "init commit" --quiet
+git remote add origin $FUNCTIONAL_REPO
+git push -u origin master --quiet
+if [ $? -eq 0 ]
+then
+  echo "Code push success on '$LZ_REPOPREFIX-gcp-functional-assuran' repository"
+else
+  echo "Failed to push code into '$LZ_REPOPREFIX-gcp-functional-assuran' repository"
+fi
+cd ..
+
+#nft - performance repo
+cd gcpworkshop-tests-performance
+git init
+git add .
+git commit -m "init commit" --quiet
+git remote add origin $PERFORMANCE_REPO
+git push -u origin master --quiet
+if [ $? -eq 0 ]
+then
+  echo "Code push success on '$LZ_REPOPREFIX-gcp-tests-performance' repository"
+else
+  echo "Failed to push code into '$LZ_REPOPREFIX-gcp-tests-performance' repository"
+fi
+cd ..
+
+#nft - security repo
+cd gcpworkshop-tests-security
+git init
+git add .
+git commit -m "init commit" --quiet
+git remote add origin $SECURITY_REPO
+git push -u origin master --quiet
+if [ $? -eq 0 ]
+then
+  echo "Code push success on '$LZ_REPOPREFIX-gcp-tests-security' repository"
+else
+  echo "Failed to push code into '$LZ_REPOPREFIX-gcp-tests-security' repository"
+fi
+cd ..
+
+#nft - accessbility repo
+cd gcpworkshop-tests-accessibility
+git init
+git add .
+git commit -m "init commit" --quiet
+git remote add origin $ACCESSIBILITY_REPO
+git push -u origin master --quiet
+if [ $? -eq 0 ]
+then
+  echo "Code push success on '$LZ_REPOPREFIX-gcp-tests-accessibility' repository"
+else
+  echo "Failed to push code into '$LZ_REPOPREFIX-gcp-tests-accessibility' repository"
+fi
+
+echo "
++----------------------------------------------------------------------------+
+| All the code repositories are now pushed to your GCP cloud source          |
++----------------------------------------------------------------------------+
+"
