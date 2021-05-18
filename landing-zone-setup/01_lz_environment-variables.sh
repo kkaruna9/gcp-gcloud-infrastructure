17d3b997955658f205c3eb381a50d5d92b1e6c7f
Dinesh.A <a.dinesh1901@gmail.com>
2021-05-18 13:11:32 +0100

    Committing the changes for landingzone infrastructure setup - environment variables cleanup

--- landing-zone-setup/01_lz_environment-variables.sh
+++ landing-zone-setup/01_lz_environment-variables.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+echo "
++----------------------------------------------------------------------------+
+| Environment variable creation  '$LZ_PROJECT_ID '.                       |
++----------------------------------------------------------------------------+
+"
+
+# ßgit clone https://github.com/dinesh-akrishnan/gcp-gcloud-infrastructure.git
+# cd gcp-gcloud-infrastructure
+# cd landing-zone-setup
+# sh ./01_lz_environment-variables.sh
+# sh ./02_lz_create-repo.sh
 export LZ_PROJECT_ID=$(gcloud config get-value project)
 export LZ_REPOPREFIX=lz-repo-1
 export LZ_BUCKET_NAME=lz-results
