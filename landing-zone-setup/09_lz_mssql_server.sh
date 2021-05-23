c986c8139a62fa6c50cd9d02d90617481fc742b3
Dinesh.A <a.dinesh1901@gmail.com>
2021-05-23 21:49:11 +0100

    sonar namespace

--- landing-zone-setup/09_lz_mssql_server.sh
+++ landing-zone-setup/09_lz_mssql_server.sh
@@ -9,7 +9,7 @@ echo "
 
 helm repo update
 helm install --set acceptEula.value=Y --set edition.value=Developer stable/mssql-linux --generate-name
-printf $(kubectl get secret --namespace default mssql-linux-1621781957-secret -o jsonpath="{.data.sapassword}" | base64 --decode);echo
+#Sprintf $(kubectl get secret --namespace default mssql-linux-1621781957-secret -o jsonpath="{.data.sapassword}" | base64 --decode);echo
 #Sou0U2rdylSLEJADHyOL
 #kubectl run mssqlcli --image=microsoft/mssql-tools -ti --restart=Never --rm=true -- /bin/bash
 #sqlcmd -S mssql-linux-1621781957.default,1433 -U sa
