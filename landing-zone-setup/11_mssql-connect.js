cc781a717ec0b502e70737a4b44167bcbf4f67a4
Dinesh.A <a.dinesh1901@gmail.com>
2021-05-26 05:52:36 +0100

    nmp connect to database - ip address

---
+++ landing-zone-setup/11_mssql-connect.js
@@ -0,0 +1,50 @@
+/*var sh = require('shelljs');
+const fs = require('fs');
+const sql = require('mssql');
+
+let mstackToken = fs.readFileSync('./mstack.json');
+let mstackTokenJson = JSON.parse(mstackToken);
+let external_ip_db = mstackTokenJson.MSSQL_IP;
+let token = mstackTokenJson.PAT;
+let organizationName = mstackTokenJson.ORGANIZATION_NAME;
+
+sh.exec(`export AZURE_DEVOPS_EXT_PAT=${token}`, {async: false, silent:true}).stdout;
+sh.exec(`export myorganization=${organizationName}`, {async: false, silent:true}).stdout;
+*/
+
+const config = {
+    user: 'sa',
+    password: 'Ctsshop@db',
+    server: '35.237.14.90',
+    options: {
+        "enableArithAbort": true
+    },
+    requestTimeout: 10000,
+ }
+
+createDB();
+useDB();
+
+function createDB() {
+    sql.connect(config).then(function () {
+        var request = new sql.Request();
+        request.query("create database ctsshop").then(function (resp) {
+            sql.close();
+        }).catch(function (err) {
+            sql.close();
+        });
+    }).catch(function (err) {
+    });
+}
+
+function useDB() {
+        sql.connect(config).then(function () {
+            var request = new sql.Request();
+            request.query("USE ctsshop").then(function (resp) {
+                sql.close();
+            }).catch(function (err) {
+                sql.close();
+            });
+        }).catch(function (err) {
+        });
+}
