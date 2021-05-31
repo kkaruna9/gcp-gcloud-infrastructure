17d19c30daf1cb3910a2bbf8933396695039db0f
Dinesh.A <a.dinesh1901@gmail.com>
2021-05-31 19:18:42 +0100

    SonarQube namespace

--- landing-zone-setup/09_lz_sonarQube.sh
+++ landing-zone-setup/09_lz_sonarQube.sh
@@ -9,7 +9,8 @@ echo "
 # gcloud container clusters get-credentials spinnaker-ci-cd --zone us-east1-c --project $PROJECT_ID
 gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA --zone $DEFAULT_ZONE --project $PROJECT_ID
 kubectl create namespace $KUBECTL_SONARQUBE
-kubectl run $KUBECTL_SONARQUBE --image=sonarqube:7.5-community  --requests='cpu=0.75,memory=1250Mi'
+kubectl get namespace
+kubectl run $KUBECTL_SONARQUBE --image=sonarqube:7.5-community  --requests='cpu=0.75,memory=1250Mi' --namespace=$KUBECTL_SONARQUBE
 kubectl expose pod $KUBECTL_SONARQUBE --port=6002 --target-port=9000 --name=$KUBECTL_SONARQUBE --type=LoadBalancer
 
 
