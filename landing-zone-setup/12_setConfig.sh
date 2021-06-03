dd3c3e3a90cd36ca6da69ae0e81775970c47ba5c
Dinesh.A <a.dinesh1901@gmail.com>
2021-06-03 06:59:34 +0100

    Default config name value assigned

--- landing-zone-setup/12_setConfig.sh
+++ landing-zone-setup/12_setConfig.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 echo "
 +----------------------------------------------------------------------------+
-|                   Environment variables configuration                            |
+|                   Runtime Enviornments Variables configuration             |
 +----------------------------------------------------------------------------+
 "
 
@@ -16,4 +16,12 @@ DEFAULT_MSSQL_IP=$(kubectl get svc $KUBECTL_MSSQL --template="{{range .status.lo
 echo "DEFAULT_MSSQL_IP = "$DEFAULT_MSSQL_IP
 
 DEFAULT_DASHBOARD_IP=$(kubectl get svc KUBECTL_RDASHBAORD --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}");
-echo "DEFAULT_SONAR_IP = "$DEFAULT_DASHBOARD_IP"
+echo "DEFAULT_SONAR_IP = "$DEFAULT_DASHBOARD_IP
+
+gcloud runtime-config configs create $DEFAULT_CONFIG_NAME --description "Global config that hold major key value pairs"
+gcloud beta runtime-config configs variables set DEFAULT_SONAR_IP $DEFAULT_SONAR_IP --config-name $DEFAULT_CONFIG_NAME --is-text
+gcloud beta runtime-config configs variables set DEFAULT_GRID_IP $DEFAULT_GRID_IP --config-name $DEFAULT_CONFIG_NAME --is-text
+gcloud beta runtime-config configs variables set DEFAULT_MSSQL_IP $DEFAULT_MSSQL_IP --config-name $DEFAULT_CONFIG_NAME --is-text
+gcloud beta runtime-config configs variables set DEFAULT_DASHBOARD_IP $DEFAULT_DASHBOARD_IP --config-name $DEFAULT_CONFIG_NAME --is-text
+#gcloud beta runtime-config configs variables get-value DEFAULT_SONAR_IP --config-name $DEFAULT_CONFIG_NAME
+#gcloud beta runtime-config configs variables unset DEFAULT_SONAR_IP --config-name $DEFAULT_CONFIG_NAME
--- landing-zone-teardown/00_Cleanup_and_check.sh
+++ landing-zone-teardown/00_Cleanup_and_check.sh
@@ -2,3 +2,6 @@ rm -rf gcp-gcloud-infrastructure
 rm -rf gcpworkshop-code-repos
 rm -rf click-to-deploy/
 rm -rf temp
+
+
+gcloud beta runtime-config configs delete $DEFAULT_CONFIG_NAME
