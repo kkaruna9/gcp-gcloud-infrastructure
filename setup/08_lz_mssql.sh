e896996bfacbb2e38a125baa766dc690d7a3b340
Dinesh.A <a.dinesh1901@gmail.com>
2021-06-15 12:10:34 +0100

    updated the namespace for the kubectl service - mssql

--- setup/08_lz_mssql.sh
+++ setup/08_lz_mssql.sh
@@ -10,11 +10,11 @@ echo "
 gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA --zone $DEFAULT_ZONE --project $PROJECT_ID
 kubectl create namespace $KUBECTL_MSSQL
 kubectl get namespace
-kubectl apply -f  "temp/gcp-gcloud-infrastructure/setup/mssql.yml"
+kubectl apply -f  "temp/gcp-gcloud-infrastructure/setup/mssql.yml" --namespace=$KUBECTL_MSSQL
 bash -c external_ip="";
 while [ -z $external_ip ];
 do echo "Please Wait mssql-container Loading...";
-external_ip=$(kubectl get svc mssql-container --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}");
+external_ip=$(kubectl get svc mssql-container  --namespace=$KUBECTL_MSSQL --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}");
 [ -z "$external_ip" ] && sleep 15; done; echo "End point ready-" && echo $external_ip; export endpoint=$external_ip
 
 #kubectl get pod mssql-container --template='{{(index (index .spec.containers 0).ports 0).containerPort}}{{"\n"}}'
