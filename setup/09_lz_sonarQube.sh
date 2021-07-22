50e6d0973f8b44428a7615a45b39e88f26eeff54
Dinesh.A <a.dinesh1901@gmail.com>
2021-07-22 15:48:02 +0100

    sonarQube removed namespace

--- setup/09_lz_sonarQube.sh
+++ setup/09_lz_sonarQube.sh
@@ -8,13 +8,17 @@ echo "
 
 # gcloud container clusters get-credentials spinnaker-ci-cd --zone us-east1-c --project $PROJECT_ID
 gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA --zone $DEFAULT_ZONE --project $PROJECT_ID
-kubectl create namespace $KUBECTL_SONARQUBE
-kubectl get namespace
+#kubectl create namespace $KUBECTL_SONARQUBE
+#kubectl get namespace
 # kubectl describe po -n $KUBECTL_SONARQUBE $KUBECTL_SONARQUBE
 #kubectl run $KUBECTL_SONARQUBE --image=sonarqube:7.5-community --requests='cpu=0.75,memory=1250Mi'
-kubectl run $KUBECTL_SONARQUBE --image=sonarqube:7.5-community --namespace=$KUBECTL_SONARQUBE
+#kubectl run $KUBECTL_SONARQUBE --image=sonarqube:7.5-community --namespace=$KUBECTL_SONARQUBE
 #kubectl expose pod $KUBECTL_SONARQUBE --port=6002 --targetport=9000 --name=$KUBECTL_SONARQUBE --type=LoadBalancer
-kubectl expose pod $KUBECTL_SONARQUBE --namespace=$KUBECTL_SONARQUBE --port=6002 --target-port=9000 --name=$KUBECTL_SONARQUBE --type=LoadBalancer
+#kubectl expose pod $KUBECTL_SONARQUBE --namespace=$KUBECTL_SONARQUBE --port=6002 --target-port=9000 --name=$KUBECTL_SONARQUBE --type=LoadBalancer
+
+kubectl run $KUBECTL_SONARQUBE --image=sonarqube:7.5-community
+#kubectl expose pod $KUBECTL_SONARQUBE --port=6002 --targetport=9000 --name=$KUBECTL_SONARQUBE --type=LoadBalancer
+kubectl expose pod $KUBECTL_SONARQUBE --port=6002 --target-port=9000 --name=$KUBECTL_SONARQUBE --type=LoadBalancer
 
 
 
