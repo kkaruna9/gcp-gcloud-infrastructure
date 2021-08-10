d548e01debf3910e64ac3e4bca77bfdfcdd13d96
Dinesh.A <a.dinesh1901@gmail.com>
2021-08-10 19:14:15 +0100

    updated the landing zone path for the deploy-services-frontend-to-gke

--- setup/13_postbuildqa.sh
+++ setup/13_postbuildqa.sh
@@ -10,7 +10,7 @@ export POST_BUILD_FRONTEND_QA="gcp-frontend"
 export POST_BUILD_FRONTEND_QA_DEST_LOCAL_PATH="temp/gcp-frontend"
 
 gcloud source repos clone $POST_BUILD_FRONTEND_QA $POST_BUILD_FRONTEND_QA_DEST_LOCAL_PATH --project=$PROJECT_ID
-source $PRE_BUILD_LANDINGZONE_QA/deploy-services-frontend-to-gke.sh
+source $POST_BUILD_FRONTEND_QA_DEST_LOCAL_PATH/deploy-services-frontend-to-gke.sh
 
 
 export LANDING_ZONE_FUNCTIONAL="landing-zone-functional"
