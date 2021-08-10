1462602812ab6c60646cce2f66558b22fd1937cf
Dinesh.A <a.dinesh1901@gmail.com>
2021-08-10 19:53:26 +0100

    updated npm command

--- setup/13_postbuildqa.sh
+++ setup/13_postbuildqa.sh
@@ -17,4 +17,4 @@ source $PRE_BUILD_LANDINGZONE_QA_DEST_LOCAL_PATH/deploy-services-frontend-to-gke
 export LANDING_ZONE_FUNCTIONAL="landing-zone-functional"
 export LANDING_ZONE_FUNCTIONAL_QA_DEST_LOCAL_PATH="temp/landing-zone-functional"
 gcloud source repos clone $LANDING_ZONE_FUNCTIONAL $LANDING_ZONE_FUNCTIONAL_QA_DEST_LOCAL_PATH --project=$PROJECT_ID
-npm run --prefix $LANDING_ZONE_FUNCTIONAL_QA_DEST_LOCAL_PATH ${"smokeTest"}
+npm run --prefix $LANDING_ZONE_FUNCTIONAL_QA_DEST_LOCAL_PATH smokeTest
--- setup/14_functionalqa.sh
+++ setup/14_functionalqa.sh
@@ -7,4 +7,4 @@ echo "
 export LANDING_ZONE_FUNCTIONAL="landing-zone-functional"
 export LANDING_ZONE_FUNCTIONAL_QA_DEST_LOCAL_PATH="temp/landing-zone-functional"
 #gcloud source repos clone $LANDING_ZONE_FUNCTIONAL $LANDING_ZONE_FUNCTIONAL_QA_DEST_LOCAL_PATH --project=$PROJECT_ID
-npm run --prefix $LANDING_ZONE_FUNCTIONAL_QA_DEST_LOCAL_PATH ${"functional"}
+npm run --prefix $LANDING_ZONE_FUNCTIONAL_QA_DEST_LOCAL_PATH functional
