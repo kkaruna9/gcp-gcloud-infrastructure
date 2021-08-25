172fdc69b3f3e08004327f9319023273a5e192a6
Dinesh.A <a.dinesh1901@gmail.com>
2021-08-25 16:55:13 +0100

    updated services in the config

--- setup/06_lz_create_container_registry.sh
+++ setup/06_lz_create_container_registry.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "
-+----------------------------------------------------------------------------+
-|                   Enabling container registry services                     |
-+----------------------------------------------------------------------------+
++-----------------------------------------------------------------------------------+
+|     Enabling container registry, container, cloudbuild, runtimeconfig services    |
++-----------------------------------------------------------------------------------+
 "
 gcloud services enable containerregistry.googleapis.com
+gcloud services enable container.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable runtimeconfig.googleapis.com
