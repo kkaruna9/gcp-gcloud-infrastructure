d438535ce4da968e761670277c791b0fcc3630ac
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-15 05:19:04 +0100

    Workshop  two cluster zone xhange

--- setup/06_lz_create_container_registry.sh
+++ setup/06_lz_create_container_registry.sh
@@ -4,14 +4,14 @@ echo "
 |     Enabling container registry, container, cloudbuild, runtimeconfig services    |
 +-----------------------------------------------------------------------------------+
 "
-gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA_SERVER --zone $DEFAULT_ZONE --project $PROJECT_ID
+gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA_SERVER --zone $DEFAULT_ZONE_SERVER --project $PROJECT_ID
 
 gcloud services enable containerregistry.googleapis.com
 gcloud services enable container.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable runtimeconfig.googleapis.com
 
-gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA_APP --zone $DEFAULT_ZONE --project $PROJECT_ID
+gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA_APP --zone $DEFAULT_ZONE_APP --project $PROJECT_ID
 
 gcloud services enable containerregistry.googleapis.com
 gcloud services enable container.googleapis.com
