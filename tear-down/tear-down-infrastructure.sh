c892d33a37acf8c5969310a3ccb2909b3727052c
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-16 05:41:45 +0100

    tear down updated

--- tear-down/tear-down-infrastructure.sh
+++ tear-down/tear-down-infrastructure.sh
@@ -87,8 +87,8 @@ fi
 
 
 echo "<<<<<<<<<<<<<<<<<<<< Tear Down: GCP Kubernetes Cluster >>>>>>>>>>>>>>>>>>>>>>>"
-gcloud container clusters delete $DEFAULT_CLUSTER_QEA_SERVER --region=$DEFAULT_ZONE --async --quiet &
-gcloud container clusters delete $DEFAULT_CLUSTER_QEA_APP --region=$DEFAULT_ZONE --async --quiet &
+gcloud container clusters delete $DEFAULT_CLUSTER_QEA_SERVER --region=$DEFAULT_ZONE_SERVER --async --quiet &
+gcloud container clusters delete $DEFAULT_CLUSTER_QEA_APP --region=$DEFAULT_ZONE_APP --async --quiet &
 if [ $? -eq 0 ]
 then
   echo "Clusters Deleted Successfully  '$DEFAULT_CLUSTER_QEA_SERVER'  & '$DEFAULT_CLUSTER_QEA_APP'"
