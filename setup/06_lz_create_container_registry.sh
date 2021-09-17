159427694429bb44d7aca66dd8d01e985485075e
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-17 18:21:31 +0100

    Service enabling and the variables are updated

--- setup/06_lz_create_container_registry.sh
+++ setup/06_lz_create_container_registry.sh
@@ -5,35 +5,15 @@ echo "
 +-----------------------------------------------------------------------------------+
 "
 
-gcloud services enable sourcerepo.googleapis.com
-gcloud services enable cloudresourcemanager.googleapis.com
-gcloud services enable compute.googleapis.com
-gcloud services enable container.googleapis.com
-gcloud services enable containerregistry.googleapis.com
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable iam.googleapis.com
-gcloud services enable logging.googleapis.com
-gcloud services enable monitoring.googleapis.com
-gcloud services enable storage-api.googleapis.com
-gcloud services enable storage-component.googleapis.com
-gcloud services enable sourcerepo.googleapis.com
-gcloud services enable pubsub.googleapis.com
-gcloud services enable runtimeconfig.googleapis.com
 
 
 gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA_SERVER --zone $DEFAULT_ZONE_SERVER --project $PROJECT_ID
 
-gcloud services enable containerregistry.googleapis.com
-gcloud services enable container.googleapis.com
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable runtimeconfig.googleapis.com
+
 
 gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA_APP --zone $DEFAULT_ZONE_APP --project $PROJECT_ID
 
-gcloud services enable containerregistry.googleapis.com
-gcloud services enable container.googleapis.com
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable runtimeconfig.googleapis.com
+
 
 #gcloud container clusters get-credentials $DEFAULT_CLUSTER_QEA_CICD --zone $DEFAULT_ZONE --project $PROJECT_ID
 
