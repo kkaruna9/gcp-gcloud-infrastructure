a19db03bd007cb2cbe0dd812f4e9a847873d2a5b
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-18 08:44:20 +0100

    account setup

---
+++ setup/00_gcp_initialSetup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+echo "
++----------------------------------------------------------------------------+
+|                   GCP Initial Project Setup                            |
++----------------------------------------------------------------------------+
+"
+echo -n "Do you want to create a new project (y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    read -p 'Project Name :' PROJECT_NAME
+    gcloud projects create $PROJECT_NAME
+else
+    echo No
+fi
+
+echo -n "Create a service account with owner role (y/n)? "
+read san
+if [ "$san" != "${san#[Yy]}" ] ;then
+    read -p 'Enter Service Account Name :' SA_NAME
+    gcloud iam service-accounts create $SA_NAME
+    PROJECT_ID=$(gcloud config get-value project)
+    gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:"$SA_NAME"@"$PROJECT_ID".iam.gserviceaccount.com" --role="roles/owner"
+    gcloud iam service-accounts keys create $PROJECT_ID.json --iam-account=$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com
+    export GOOGLE_APPLICATION_CREDENTIALS="temp/service-account-file.json"
+else
+    echo Service Account setup is done by user Manually
+fi
+
+
+
+
+
+
+
