fe49ca001a3a8155f31f4b94a79c30d8b182be4e
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-20 17:10:20 +0100

    tear-down firewall rules

---
+++ tear-down/04_tear-down-firewall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+echo "<<<<<<<<<<<<<<<<<<<< Tear Down: GCP Tear Firewall Rules >>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+gcloud compute firewall-rules delete sonar-firewall-rule report-firewall-rule zalenium-firewall-rule
