6bd44ab2b87f66216278a22473d99c4c1961bf84
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-20 18:33:37 +0100

    Vm approach changes - params within the ssh vm

--- setup/01_vm_setup.sh
+++ setup/01_vm_setup.sh
@@ -19,21 +19,22 @@ fi
 gcloud beta compute --project=$PROJECT_ID instances create $DEFAULT_CLUSTER_QEA_SERVER --zone=$DEFAULT_ZONE_SERVER --machine-type=e2-standard-8 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=$SA_NAME --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --image=debian-10-buster-v20210916 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-balanced --boot-disk-device-name=$DEFAULT_CLUSTER_QEA_SERVER --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
 sleep 60
 gcloud compute instances list
-gcloud compute ssh --zone $DEFAULT_ZONE_SERVER $DEFAULT_CLUSTER_QEA_SERVER --project $PROJECT_ID
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
-sudo apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
-sudo docker info
-sudo docker run -d --name sonarqube -p 6002:9000 sonarqube:7.5-community
-sudo docker run --name reportingdashboard -p 3337:3337 --rm -i -t -d karthiknarayanpdec11/dashboardgcp:latest
-sudo docker pull elgalu/selenium
-sudo docker pull dosel/zalenium
-sudo docker run --rm -ti --name zalenium -p 4444:4444 -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/videos:/home/seluser/videos --privileged dosel/zalenium start -d
+gcloud compute ssh --zone $DEFAULT_ZONE_SERVER $DEFAULT_CLUSTER_QEA_SERVER --project $PROJECT_ID --' sudo apt update &&
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common &&
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - &&
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" &&
+sudo apt update &&
+sudo apt-cache policy docker-ce &&
+sudo apt install docker-ce &&
+sudo systemctl status docker &&
+sudo docker info &&
+sudo docker run -d --name sonarqube -p 6002:9000 sonarqube:7.5-community &&
+sudo docker run --name reportingdashboard -p 3337:3337 --rm -i -t -d karthiknarayanpdec11/dashboardgcp:latest &&
+sudo docker pull elgalu/selenium &&
+sudo docker pull dosel/zalenium &&
+sudo docker run --rm -ti --name zalenium -p 4444:4444 -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/videos:/home/seluser/videos --privileged dosel/zalenium start -d &&
+ logout
+ '
 gcloud compute firewall-rules create sonar-firewall-rule --allow tcp:6002 --source-tags=$DEFAULT_CLUSTER_QEA_SERVER --source-ranges=0.0.0.0/0 --description="For testing purpose"
 gcloud compute firewall-rules create report-firewall-rule --allow tcp:3337 --source-tags=$DEFAULT_CLUSTER_QEA_SERVER --source-ranges=0.0.0.0/0 --description="For testing purpose"
 gcloud compute firewall-rules create zalenium-firewall-rule --allow tcp:4444 --source-tags=$DEFAULT_CLUSTER_QEA_SERVER --source-ranges=0.0.0.0/0 --description="For testing purpose"
