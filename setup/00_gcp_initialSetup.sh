aa008f51f0882fbc9d50a0c12e5e5b3926c6d7f2
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-21 18:00:47 +0100

    Teardown - scripts

--- setup/00_gcp_initialSetup.sh
+++ setup/00_gcp_initialSetup.sh
@@ -4,10 +4,12 @@ echo "
 |                   GCP Initial Project Setup                            |
 +----------------------------------------------------------------------------+
 "
+: '
 echo -n "Do you want to create a new project (y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    read -p 'Project Name :' PROJECT_NAME
+    read -p "Project Name :" PROJECT_NAME
+
     gcloud projects create $PROJECT_NAME --set-as-default
     gcloud config set project $PROJECT_NAME
 else
@@ -18,8 +20,9 @@ fi
 echo -n "Create a service account with owner role (y/n)? "
 read san
 if [ "$san" != "${san#[Yy]}" ] ;then
-    read -p 'Enter Service Account Name :' SA_NAME
-    gcloud iam service-accounts create $SA_NAME
+    read -p "Enter Service Account Name :" SA_NAME
+    userEmail=$(gcloud auth list --format="value(account)")
+    gcloud iam service-accounts create $SA_NAME --display-name="My Service Account"
     PROJECT_ID=$(gcloud config get-value project)
     gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:"$SA_NAME"@"$PROJECT_ID".iam.gserviceaccount.com" --role="roles/owner"
     gcloud iam service-accounts keys create $PROJECT_ID.json --iam-account=$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com
@@ -30,6 +33,23 @@ else
     red=`tput setaf 1`
     echo  $red ALERT !  Service Account setup is done by user Manually
 fi
+
+'
+
+PROJECT_NAME="gcp-digital-shopify"
+SA_NAME="sa-digital-shopify"
+gcloud projects create $PROJECT_NAME --set-as-default
+gcloud config set project $PROJECT_NAME
+userEmail=$(gcloud auth list --format="value(account)")
+gcloud iam service-accounts create $SA_NAME --display-name="Digital Shopify ServiceAccount"
+PROJECT_ID=$(gcloud config get-value project)
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:"$SA_NAME"@"$PROJECT_ID".iam.gserviceaccount.com" --role="roles/owner"
+gcloud iam service-accounts keys create $PROJECT_ID.json --iam-account=$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com
+mv $PROJECT_ID.json temp
+export SA_NAME=$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com
+export GOOGLE_APPLICATION_CREDENTIALS="temp/"$PROJECT_ID.json
+CURRENT_BILLING_ACCOUNT_ID=$(gcloud beta billing accounts list --format="value(name)")
+gcloud alpha billing accounts projects link $PROJECT_ID --billing-account=$CURRENT_BILLING_ACCOUNT_ID
 
 
 
--- setup/00_lz_environment-variables.sh
+++ setup/00_lz_environment-variables.sh
@@ -108,34 +108,4 @@ git config --global user.name $GIT_USERNAME
 npm install mssql
 npm install shelljs
 
-echo "
-+----------------------------------------------------------------------------+
-|     Enabling the Google Cloud Services for the workshop                    |
-+----------------------------------------------------------------------------+
-"
-gcloud services enable sourcerepo.googleapis.com
-gcloud services enable cloudresourcemanager.googleapis.com
-gcloud services enable compute.googleapis.com
-gcloud services enable container.googleapis.com
-gcloud services enable containerregistry.googleapis.com
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable iam.googleapis.com
-gcloud services enable logging.googleapis.com
-gcloud services enable monitoring.googleapis.com
-gcloud services enable storage-api.googleapis.com
-gcloud services enable storage-component.googleapis.com
-gcloud services enable sourcerepo.googleapis.com
-gcloud services enable pubsub.googleapis.com
-gcloud services enable runtimeconfig.googleapis.com
-
-gcloud services enable containerregistry.googleapis.com
-gcloud services enable container.googleapis.com
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable runtimeconfig.googleapis.com
-
-gcloud services enable containerregistry.googleapis.com
-gcloud services enable container.googleapis.com
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable runtimeconfig.googleapis.com
-
 
