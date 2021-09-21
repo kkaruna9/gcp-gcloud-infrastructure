ef316b11bcd5ea70e7703fa5877427f75e09ad1c
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-21 17:57:30 +0100

    Teardown - scripts

--- tear-down/04_tear-down-firewall.sh
+++ tear-down/04_tear-down-firewall.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "<<<<<<<<<<<<<<<<<<<< Tear Down: GCP Tear Firewall Rules >>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 gcloud compute firewall-rules delete sonar-firewall-rule report-firewall-rule zalenium-firewall-rule
+if [ $? -eq 0 ]
+then
+  echo "Deleted Firewall Rules"
+else
+  echo "Failed to Delete Firewall Rules"
+fi
---
+++ tear-down/07_tear-down-folders.sh
@@ -0,0 +1,10 @@
+
+echo "<<<<<<<<<<<<<<<<<<<< Tear Down: GCP Project folders  >>>>>>>>>>>>>>>>>>>>>>>>>"
+rm -rf temp/
+rm -rf *
+if [ $? -eq 0 ]
+then
+  echo "Deleted all folders in the current directory"
+else
+  echo "Deleted all folders in the current directory"
+fi
