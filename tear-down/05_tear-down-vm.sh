5bce170570e68136b5bd490cca134cde4e9f222d
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-27 17:59:32 +0100

    quiet update

--- tear-down/05_tear-down-vm.sh
+++ tear-down/05_tear-down-vm.sh
@@ -4,7 +4,7 @@ echo "<<<<<<<<<<<<<<<<<<<< Tear Down: GCP VM Instance >>>>>>>>>>>>>>>>>>>>>>>>>>
 source temp/gcp-gcloud-infrastructure/setup/02_lz_environment-variables.sh
 
 
-gcloud compute instances delete $DEFAULT_CLUSTER_QEA_SERVER --zone=$DEFAULT_ZONE_SERVER
+gcloud compute instances delete $DEFAULT_CLUSTER_QEA_SERVER --zone=$DEFAULT_ZONE_SERVER --quiet
 if [ $? -eq 0 ]
 then
   echo "Deleted VM Instance '$DEFAULT_CLUSTER_QEA_SERVER' from zone '$DEFAULT_ZONE_SERVER'"
--- tear-down/06_tear-down-clusters.sh
+++ tear-down/06_tear-down-clusters.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 source temp/gcp-gcloud-infrastructure/setup/02_lz_environment-variables.sh
 
-gcloud container clusters delete $DEFAULT_CLUSTER_QEA_SERVER --region=$DEFAULT_ZONE_SERVER --async --quiet &
-gcloud container clusters delete $DEFAULT_CLUSTER_QEA_APP --region=$DEFAULT_ZONE_APP --async --quiet &
+#gcloud container clusters delete $DEFAULT_CLUSTER_QEA_SERVER --region=$DEFAULT_ZONE_SERVER --async --quiet
+gcloud container clusters delete $DEFAULT_CLUSTER_QEA_APP --region=$DEFAULT_ZONE_APP --async --quiet
 if [ $? -eq 0 ]
 then
   echo "Clusters Deleted Successfully  '$DEFAULT_CLUSTER_QEA_SERVER'  & '$DEFAULT_CLUSTER_QEA_APP'"
