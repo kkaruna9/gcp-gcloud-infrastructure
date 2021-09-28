0a9a66e24cf45afa8d6891bc0ef7ce995168d3d0
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-28 05:57:43 +0100

    delete service account

--- tear-down/07_tear-down-serviceaccount.sh
+++ tear-down/07_tear-down-serviceaccount.sh
@@ -2,7 +2,7 @@
 echo "<<<<<<<<<<<<<<<<<<<< Tear Down: GCP Tear-Down Service Accounts >>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 source temp/gcp-gcloud-infrastructure/setup/02_lz_environment-variables.sh
 
-gcloud iam service-accounts delete $SERVICE_ACCOUNT
+gcloud iam service-accounts delete $SERVICE_ACCOUNT --quiet
 if [ $? -eq 0 ]
 then
   echo "Service Account '$SERVICE_ACCOUNT' is deleted"
