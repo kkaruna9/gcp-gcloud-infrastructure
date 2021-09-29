d801cac7627143de92a510a5e58f903abc8b240f
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-29 11:01:33 +0100

    updated the environment variables comments

--- setup/02_lz_environment-variables.sh
+++ setup/02_lz_environment-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "
 +----------------------------------------------------------------------------+
-|                   Environment variable creation                            |
+|                   Environment variables                                    |
 +----------------------------------------------------------------------------+
 "
 # git clone https://github.com/dinesh-akrishnan/gcp-gcloud-infrastructure.git
