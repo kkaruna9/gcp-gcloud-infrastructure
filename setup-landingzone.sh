16166ac5be76a8cbfbe957882a67cee05a72a2b2
Dinesh.A <a.dinesh1901@gmail.com>
2021-09-29 10:12:53 +0100

    git user updated

--- setup-landingzone.sh
+++ setup-landingzone.sh
@@ -4,17 +4,17 @@ echo "
 |                   GCP - OneShot - Infrastructure Setup                     |
 +----------------------------------------------------------------------------+
 "
-source temp/gcp-gcloud-infrastructure/setup/00_gcp_initialSetup.sh
-source temp/gcp-gcloud-infrastructure/setup/01_gcp_services.sh
-source temp/gcp-gcloud-infrastructure/setup/02_lz_environment-variables.sh
+  source temp/gcp-gcloud-infrastructure/setup/00_gcp_initialSetup.sh
+  source temp/gcp-gcloud-infrastructure/setup/01_gcp_services.sh
+  source temp/gcp-gcloud-infrastructure/setup/02_lz_environment-variables.sh
 
-source temp/gcp-gcloud-infrastructure/setup/03_lz_create-repo.sh
-source temp/gcp-gcloud-infrastructure/setup/04_lz_create_kubectl_clusters.sh
-source temp/gcp-gcloud-infrastructure/setup/07_lz_create-bucket.sh
-#wait
-source temp/gcp-gcloud-infrastructure/setup/06_vm_setup.sh
-source temp/gcp-gcloud-infrastructure/setup/05_push_source2workshop.sh
-#wait
-source temp/gcp-gcloud-infrastructure/setup/08_lz_mssql.sh
-source temp/gcp-gcloud-infrastructure/setup/09_setConfig.sh
+  source temp/gcp-gcloud-infrastructure/setup/03_lz_create-repo.sh
+  source temp/gcp-gcloud-infrastructure/setup/04_lz_create_kubectl_clusters.sh
+  source temp/gcp-gcloud-infrastructure/setup/07_lz_create-bucket.sh
+  #wait
+  source temp/gcp-gcloud-infrastructure/setup/06_vm_setup.sh
+  source temp/gcp-gcloud-infrastructure/setup/05_push_source2workshop.sh
+  #wait
+  source temp/gcp-gcloud-infrastructure/setup/08_lz_mssql.sh
+  source temp/gcp-gcloud-infrastructure/setup/09_setConfig.sh
 
