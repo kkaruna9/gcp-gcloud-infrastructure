6ca9e9ac2de3bf82e0dccd0a1d7f426c91b7d311
Dinesh.A <a.dinesh1901@gmail.com>
2021-10-01 06:22:10 +0100

    change sequence

--- setup-landingzone.sh
+++ setup-landingzone.sh
@@ -7,8 +7,13 @@ echo "
   source temp/gcp-gcloud-infrastructure/setup/00_gcp_initialSetup.sh
   source temp/gcp-gcloud-infrastructure/setup/01_gcp_services.sh
   source temp/gcp-gcloud-infrastructure/setup/02_lz_environment-variables.sh
-
-  echo parallel:
+  echo store:
+  {
+    source temp/gcp-gcloud-infrastructure/setup/03_lz_create-repo.sh &
+    source temp/gcp-gcloud-infrastructure/setup/07_lz_create-bucket.sh
+    wait
+  }
+  echo heart:
   {
     source temp/gcp-gcloud-infrastructure/setup/03_lz_create-repo.sh &
     source temp/gcp-gcloud-infrastructure/setup/04_lz_create_kubectl_clusters.sh &
@@ -16,11 +21,11 @@ echo "
     source temp/gcp-gcloud-infrastructure/setup/07_lz_create-bucket.sh
     wait
   }
-
-
-  #wait
-  source temp/gcp-gcloud-infrastructure/setup/05_push_source2workshop.sh
-  #wait
-  source temp/gcp-gcloud-infrastructure/setup/08_lz_mssql.sh
+  echo connect:
+  {
+    source temp/gcp-gcloud-infrastructure/setup/08_lz_mssql.sh
+    source temp/gcp-gcloud-infrastructure/setup/05_push_source2workshop.sh
+    wait
+  }
   source temp/gcp-gcloud-infrastructure/setup/09_setConfig.sh
 
