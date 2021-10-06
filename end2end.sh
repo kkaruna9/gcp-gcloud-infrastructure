58958f01f5c4c074c73806bd7e054c61775d542b
Dinesh.A <a.dinesh1901@gmail.com>
2021-10-06 06:13:23 +0100

    e2e

---
+++ end2end.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "
++----------------------------------------------------------------------------+
+|                   GCP - OneShot - End to End Setup                         |
++----------------------------------------------------------------------------+
+"
+
+export PRE_BUILD_LANDINGZONE_QA_DEST_LOCAL_PATH=temp/$LZ_REPOPREFIX-landing-zone-pre-buildQA
+source $PRE_BUILD_LANDINGZONE_QA_DEST_LOCAL_PATH/gcp-e2e-script.sh
