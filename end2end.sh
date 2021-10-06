ffa620b32c6478372d1d0d7a48ef651734ebeed4
Dinesh.A <a.dinesh1901@gmail.com>
2021-10-06 06:28:06 +0100

    updated the tear down for the cluser tearning

--- end2end.sh
+++ end2end.sh
@@ -4,6 +4,5 @@ echo "
 |                   GCP - OneShot - End to End Setup                         |
 +----------------------------------------------------------------------------+
 "
-
 export PRE_BUILD_LANDINGZONE_QA_DEST_LOCAL_PATH=temp/$LZ_REPOPREFIX-landing-zone-pre-buildQA
 source $PRE_BUILD_LANDINGZONE_QA_DEST_LOCAL_PATH/gcp-e2e-script.sh
